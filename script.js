79226656f8b62bcc7c8fe60ef256a3758a10b2ea
Fadhla <fadhla153@gmail.com>
2026-01-03 00:50:40 +0700

    massage via console

--- script.js
+++ script.js
@@ -23,7 +23,21 @@ document.getElementById('Hitung').onclick = function(){
     tp = parseFloat(tp);
 
     hasil = (((alinkom*3)+(alpro*3)+(pmk*3)+(radig*2)+(basdat*3)+(strukdis*2)+(tp*3))/19);
-    
-    
+     
     document.getElementById('nilai-akhir').textContent = hasil.toFixed(2);
+
+    if(hasil==4.00){
+        console.log("kurang, gak menantang");
+    } else if (hasil>=3.50){
+        console.log("kamu hebat banget, pak julio bangga");
+    } else if (hasil>=3.00){
+        console.log("cakep banget tuh nilainya");
+    } else if (hasil>=2.50){
+        console.log("dikit lagi jadi mentri tuh");
+    } else if(hasil >=2.00){
+        console.log("dikit lagi bisa jadi presiden");
+    } else {
+        console.log("smngt ngulangnya bes");
+    }
+
 };
